2b8553eed566c7fafeae796e1a45c13f50aa12d3
rakesh-nandakumar <rakeshnandakumar2005@gmail.com>
2024-09-04 15:34:57 +0530

    Update helpers/math_utils_2880_153457.py

---
+++ helpers/math_utils_2880_153457.py
@@ -0,0 +1,17 @@
+import math
+
+def calculate_area_2880(radius):
+    return math.pi * radius ** 2
+
+def fibonacci_2880(n):
+    if n <= 1:
+        return n
+    return fibonacci_2880(n-1) + fibonacci_2880(n-2)
+
+def prime_check_2880(num):
+    if num < 2:
+        return False
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+    return True
