2692d84e422d2c72bd8f45c4afa3ffea688a6b10
rakesh-nandakumar <rakeshnandakumar2005@gmail.com>
2024-10-06 15:24:48 +0530

    Update utils/string_helpers_2561_152448.py

---
+++ utils/string_helpers_2561_152448.py
@@ -0,0 +1,12 @@
+def capitalize_words_2561(text):
+    return ' '.join(word.capitalize() for word in text.split())
+
+def reverse_string_2561(text):
+    return text[::-1]
+
+def count_vowels_2561(text):
+    vowels = 'aeiou'
+    return sum(1 for char in text.lower() if char in vowels)
+
+def random_function_2561():
+    return 941
