d726eb64e7be7d57e7d8ecbc4f59564af85e58ec
rakesh-nandakumar <rakeshnandakumar2005@gmail.com>
2024-10-24 15:34:00 +0530

    Update utils/string_helpers_2380_153400.py

---
+++ utils/string_helpers_2380_153400.py
@@ -0,0 +1,12 @@
+def capitalize_words_2380(text):
+    return ' '.join(word.capitalize() for word in text.split())
+
+def reverse_string_2380(text):
+    return text[::-1]
+
+def count_vowels_2380(text):
+    vowels = 'aeiou'
+    return sum(1 for char in text.lower() if char in vowels)
+
+def random_function_2380():
+    return 881
