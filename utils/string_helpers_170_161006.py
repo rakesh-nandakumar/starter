868acfc5fa97e7368d333f4974721b77cf66c87f
rakesh-nandakumar <rakeshnandakumar2005@gmail.com>
2025-06-02 16:10:06 +0530

    Update utils/string_helpers_170_161006.py

---
+++ utils/string_helpers_170_161006.py
@@ -0,0 +1,12 @@
+def capitalize_words_170(text):
+    return ' '.join(word.capitalize() for word in text.split())
+
+def reverse_string_170(text):
+    return text[::-1]
+
+def count_vowels_170(text):
+    vowels = 'aeiou'
+    return sum(1 for char in text.lower() if char in vowels)
+
+def random_function_170():
+    return 961
