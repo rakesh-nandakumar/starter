fccfd3f440da152d77a414097b97d936550153f9
rakesh-nandakumar <rakeshnandakumar2005@gmail.com>
2025-06-08 16:00:44 +0530

    Update helpers/math_utils_111_160047.py

---
+++ helpers/math_utils_111_160047.py
@@ -0,0 +1,17 @@
+import math
+
+def calculate_area_111(radius):
+    return math.pi * radius ** 2
+
+def fibonacci_111(n):
+    if n <= 1:
+        return n
+    return fibonacci_111(n-1) + fibonacci_111(n-2)
+
+def prime_check_111(num):
+    if num < 2:
+        return False
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+    return True
